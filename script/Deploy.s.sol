a2048e50b44a415c8656236438f87a00fb4b07cc
mulmod <161062515+mulmod@users.noreply.github.com>
2024-03-18 17:26:31 -0400

    chore: fraxtal deployment

---
+++ script/Deploy.s.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: The Unlicense
+pragma solidity ^0.8.0;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { MockERC20 } from "forge-std/mocks/MockERC20.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+
+import { FraxlendPair } from "../src/contracts/FraxlendPair.sol";
+import { FraxlendPairHelper } from "../src/contracts/FraxlendPairHelper.sol";
+import { FraxlendPairDeployer } from "../src/contracts/FraxlendPairDeployer.sol";
+
+import { VariableInterestRate } from "../src/contracts/VariableInterestRate.sol";
+import { LinearInterestRate } from "../src/contracts/LinearInterestRate.sol";
+
+import { MockOracle } from "../test/mocks/MockOracle.sol";
+
+contract Token is MockERC20 {
+    function mint(uint256 amount) external virtual {
+        _mint(msg.sender, amount * 10 ** _decimals);
+    }
+}
+
+contract DeployFraxlend is Script, StdCheats {
+    function computeSalt(bytes32 initCodeHash) internal virtual returns (bytes32 salt) {
+        string[] memory ffi = new string[](3);
+        ffi[0] = "bash";
+        ffi[1] = "create2.sh";
+        ffi[2] = vm.toString(initCodeHash);
+        vm.ffi(ffi);
+        salt = vm.parseBytes32(vm.readLine(".temp"));
+        try vm.removeFile(".temp") { } catch { }
+    }
+
+    function run()
+        public
+        virtual
+        returns (
+            FraxlendPairHelper helper,
+            FraxlendPairDeployer deployer,
+            VariableInterestRate variableRate,
+            LinearInterestRate linearRate,
+            FraxlendPair pair
+        )
+    {
+        vm.startBroadcast();
+
+        if (!vm.envBool("SALT_DEPLOYMENT")) {
+            helper = new FraxlendPairHelper();
+            deployer = new FraxlendPairDeployer();
+            deployer.initialize(
+                vm.envAddress("FRAXLEND_OWNER"),
+                vm.envAddress("FRAXLEND_CIRCUIT_BREAKER"),
+                vm.envAddress("FRAXLEND_COMPTROLLER"),
+                vm.envAddress("FRAXLEND_TIMELOCK")
+            );
+            deployer.setCreationCode(type(FraxlendPair).creationCode);
+            variableRate = new VariableInterestRate();
+            linearRate = new LinearInterestRate();
+        } else {
+            helper = new FraxlendPairHelper{
+                salt: computeSalt(keccak256(type(FraxlendPairHelper).creationCode))
+            }(); // no constructor
+
+            deployer = new FraxlendPairDeployer{
+                salt: computeSalt(keccak256(type(FraxlendPairDeployer).creationCode))
+            }();
+
+            deployer.initialize(
+                vm.envAddress("FRAXLEND_OWNER"),
+                vm.envAddress("FRAXLEND_CIRCUIT_BREAKER"),
+                vm.envAddress("FRAXLEND_COMPTROLLER"),
+                vm.envAddress("FRAXLEND_TIMELOCK")
+            );
+
+            deployer.setCreationCode(type(FraxlendPair).creationCode);
+
+            variableRate = new VariableInterestRate{
+                salt: computeSalt(keccak256(type(VariableInterestRate).creationCode))
+            }(); // no constructor
+
+            linearRate = new LinearInterestRate{
+                salt: computeSalt(keccak256(type(LinearInterestRate).creationCode))
+            }(); // no constructor
+        }
+
+        if (vm.envBool("DEVNET")) {
+            // deploy mocks
+            Token xbtc = new Token();
+            xbtc.initialize("xBTC", "xBTC", 8);
+
+            Token dai = new Token();
+            dai.initialize("DAI", "DAI", 18);
+
+            console2.log("xbtc", address(xbtc));
+            console2.log("dai", address(dai));
+
+            xbtc.mint(500);
+            dai.mint(100_000_000);
+
+            pair = FraxlendPair(
+                deployer.deploy(
+                    abi.encode(
+                        address(xbtc),
+                        address(dai),
+                        new MockOracle(18, 70_000e18), // 70k
+                        new MockOracle(18, 1e18),
+                        uint256(1e18),
+                        address(variableRate),
+                        abi.encode()
+                    )
+                )
+            );
+        }
+
+        vm.stopBroadcast();
+    }
+}
+
+contract DeployFraxlendPair is Script {
+    function run() public virtual returns (FraxlendPair pair) {
+        return FraxlendPair(
+            FraxlendPairDeployer(vm.envAddress("FRAXLEND_PAIR_DEPLOYER")).deploy(
+                abi.encode(
+                    vm.envAddress("FRAXLEND_ASSET"),
+                    vm.envAddress("FRAXLEND_COLLATERAL"),
+                    vm.envAddress("ORACLE_MULTIPLY"),
+                    vm.envAddress("ORACLE_DIVIDE"),
+                    vm.envUint("ORACLE_NORMALIZATION"),
+                    vm.envAddress("RATE_CONTRACT"),
+                    vm.envBytes("RATE_INIT_DATA")
+                )
+            )
+        );
+    }
+}
